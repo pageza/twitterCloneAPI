c5e898889ce118429eb9c41ab90c07215c796cbb
pageza <zap.freelance@gmail.com>
2022-03-04 13:36:14 -0700

    adding sequelize models

---
+++ models/user.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    fname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    uname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'users',
+    modelName: 'User',
+  });
+  return User;
+};
