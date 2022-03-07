e33c387ca9a29b404748203d1fa62a04be625e8d
pageza <zap.freelance@gmail.com>
2022-03-07 09:48:30 -0700

    added routes for likes

--- routes/likes.js
+++ routes/likes.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const router = express.Router()
-const { Like, Post, User } = require('../models')
+const { Like } = require('../models')
+const { Op } = require('sequelize')
 //Create a Like
 router.post('/create', async (req, res) => {
     const { postId, userId } = req.body
     try {
-        const user = await User.findOne( { where: { uuid: userId }})
-        const post = await Post.findOne( { where: { uuid: postId }})
-        const like = await Like.create({ postId:post.id, userId:user.id })
-
+        const like = await Like.create({ postId, userId })
         return res.json(like)
     } catch (err) {
         return res.status(500).json(err)
     }
 })
-
+// Get ALL likes
+router.get('/', async (req, res) => {
+    try {
+        const likes = await Like.findAll()
+        return res.json(likes)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
+// Remove a like
+router.delete('/delete', async (req, res) => {
+    const { postId, userId } = req.body
+    try {
+        const like = await Like.findOne({
+            where: {
+                [Op.and]: [{ postId }, { userId }]
+            }
+        })
+        await like.destroy()
+        return res.json({ message: "Unliked"})
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
 module.exports = router
