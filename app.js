573a8a3d31e60acc809d60d81cb83a5b4d33c8ea
pageza <zap.freelance@gmail.com>
2022-03-13 10:15:36 -0600

    added headers for CORS access

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+})
 
 app.use('/', indexRouter)
 app.use('/api/users', usersRouter)
